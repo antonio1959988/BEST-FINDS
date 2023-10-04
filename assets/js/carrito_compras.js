c508fbd270c1c301331aa45593eb19859daa23bd
Faitxi <faitxi.1997@gmail.com>
2023-10-04 09:17:53 -0600

    Se agregan mas funciones en JS para carritoCompras

--- assets/js/carrito_compras.js
+++ assets/js/carrito_compras.js
@@ -1,34 +1,36 @@
 
+//Lista de productos que se cargarán en el carrito de compras
+let listaProductos = [ //array de objetos
+    {
+        nombre: "Lorem1",
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        costo: 621,
+        imagen: "./assets/img/carritoCompras/producto1.jpg"
+    },
+    {
+        nombre: "Lorem2",
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        costo: 621,
+        imagen: "./assets/img/carritoCompras/producto2.jpg"
+    },
+    {
+        nombre: "Lorem3",
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        costo: 621,
+        imagen: "./assets/img/carritoCompras/producto3.jpg"
+    }
+];
+
+
 
 //funcion para añadir los productos al carrito
 function agregarProductos() {
-    let carrito = document.getElementById("carrito");
-    //Lista de productos que se cargarán en el carrito de compras
-    let listaProductos = [
-        {
-            nombre: "Lorem1",
-            descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-            costo: 621,
-            imagen: "./assets/img/carritoCompras/producto1.jpg"
-        },
-        {
-            nombre: "Lorem2",
-            descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-            costo: 621,
-            imagen: "./assets/img/carritoCompras/producto2.jpg"
-        },
-        {
-            nombre: "Lorem3",
-            descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-            costo: 621,
-            imagen: "./assets/img/carritoCompras/producto3.jpg"
-        }
-    ];
+    let carrito = document.getElementById("carrito"); //necesitamos el container de carrito para agregar el row de producto
 
-    listaProductos.map((producto,index) => {
+    listaProductos.map((producto, index) => { //map permite iterar sobre los elementos del array y se le agrega un indice a cada iteracion, el indice lo necesitamos para generar un ID unico para los elementos de cada row de producto con los que vamos a interactuar
 
         let rowProducto = document.createElement("producto");
-        rowProducto.setAttribute("id",`producto-${index}`)
+        rowProducto.setAttribute("id", `producto-${index}`) //interactuamos con el row para poder eliminarlo completo
         rowProducto.className = "row rowPosition  mb-3";
         rowProducto.innerHTML = `
         <div class="col"> <!-- div  col Imagen-->
@@ -47,11 +49,11 @@ function agregarProductos() {
                     <!-- Seccion contador -->
                     <div class="d-inline-block">
                         <div class="input-group">
-                            <button id="decrementar-${index}" type="button" class="btn btn-number" onclick="decrementarCantidad(this);">
+                            <button id="decrementar-${index}" type="button" class="btn btn-number" onclick="decrementarCantidad(this); calcularPrecio(this);" > 
                                 <i class="fa-solid fa-minus"></i>
                             </button>
-                            <input type="text" id="quantity-${index}" name="quantity" class="form-control input-number" value="1" min="1" max="100">
-                            <button id="incrementar-${index}" type="button" class="btn btn-number" onclick="incrementarCantidad(this);">
+                            <input type="text" id="cantidad-${index}" name="cantidad" class="form-control input-number" value="1" min="1" max="100">
+                            <button id="incrementar-${index}" type="button" class="btn btn-number" onclick="incrementarCantidad(this); calcularPrecio(this);">
                                 <i class="fa-solid fa-plus"></i>
                             </button>
                         </div>
@@ -67,7 +69,7 @@ function agregarProductos() {
     
          <!--TOTAL PRODUCTO 1-->
         <div class="col"><!--Inicio div col3 total-->
-            <p id="cantidad-${index}">$${producto.costo}</p>
+            <p id="costo-${index}">$${producto.costo}</p>
         </div> <!--Cierre col3 total-->`;
 
         carrito.appendChild(rowProducto);
@@ -81,15 +83,17 @@ function agregarProductos() {
 
 //Funciones para el contador
 function incrementarCantidad(boton) {
-    let index = boton.id.split("-")[1];
-    let inputCantidad = document.getElementById(`quantity-${index}`);
+    let index = boton.id.split("-")[1]; //el split es para partir una palabra por medio de un caracter.
+    //Split Nos da como resultado un array de strings 
+    //["incrementar", "1"]
+    let inputCantidad = document.getElementById(`cantidad-${index}`);
     let cantidad = parseInt(inputCantidad.value);
     inputCantidad.value = cantidad + 1;
 }
 
 function decrementarCantidad(boton) {
-    let index = boton.id.split("-")[1];
-    let inputCantidad = document.getElementById(`quantity-${index}`);
+    let index = boton.id.split("-")[1]; //siempre vamos a acceder al indice uno porque es el row donde vamos a estar trabajando.
+    let inputCantidad = document.getElementById(`cantidad-${index}`);
     let cantidad = parseInt(inputCantidad.value);
     if (cantidad != 1) {
         inputCantidad.value = cantidad - 1;
@@ -98,15 +102,20 @@ function decrementarCantidad(boton) {
 
 
 //Funcion para eliminar productos
-function eliminarProducto(boton){
+function eliminarProducto(boton) {
     let index = boton.id.split("-")[1];
     let rowProducto = document.getElementById(`producto-${index}`);
     rowProducto.remove();
 }
 
 //Calcular precio
-function calcularPrecio(){
-
+function calcularPrecio(boton) {
+    let index = boton.id.split("-")[1];
+    let costo = document.getElementById(`costo-${index}`) //row
+    let inputCantidad = document.getElementById(`cantidad-${index}`);
+    let cantidad = parseInt(inputCantidad.value);
+    let calculo = listaProductos[index].costo * cantidad;
+    costo.innerHTML = calculo;
 }
 
 
