520713be48bd189bcb1a529f75e7ff0b978cd23d
Rogarzac <rogarzac@gmail.com>
2023-10-06 23:37:36 -0600

    se aplican las verificaciones de los inputs en login

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,22 +1,6 @@
 
 
-let loginButton = document.getElementById("botonL"); // Reemplaza "tuBotonDeLogin" con el ID de tu botón
-loginButton.addEventListener("click", login);
 
-
-function login() {
-    // Recuperar el valor del campo de email
-    let email = document.getElementsByName("email")[0].value;
-    
-    // Recuperar el valor del campo de contraseña
-    let password = document.getElementsByName("password")[0].value;
-    
-    // Mostrar los valores en la consola
-    console.log("Email:", email);
-    console.log("Contraseña:", password);
-    
-    // Aquí puedes agregar la lógica de autenticación con los valores recuperados
-}
 function mostrar(){
     document.getElementById('ocultar').style.display = 'block';
 }
@@ -27,38 +11,27 @@ let inputs = document.querySelectorAll('#formFormulario input');
 
 //Expresiones permitidas para cada campo
 const expresiones = {
-	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-    apellido:/^[a-zA-ZÀ-ÿ\s]{1,40}$/,
-	password: /^.{4,12}$/, // 4 a 12 digitos.
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+    password: /^.{4,12}$/, // 4 a 12 digitos.
+	correo2:/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
 }
-let campos = {
-	usuario: false,
-	nombre: false,
-	password: false,
-	correo: false,
-}
+
 
 //Switch para validar cada campo
 let validarFormulario = (e) => {
 	switch (e.target.name) {
-		case "nombre":
-			validarCampo(expresiones.nombre, e.target, 'Nombre');
-		break;
-		case "apellido":
-			validarCampo(expresiones.nombre, e.target, 'Apellido');
-		break;
-		case "password":
-			validarCampo(expresiones.password, e.target, 'Password');
-			validarPassword2();
-		break;
-		case "passwordConfirmacion":
-			validarPassword2();
-		break;
 		case "correo":
 			validarCampo(expresiones.correo, e.target, 'Correo');
 		break;
+		case "correo2":
+			validarCampo(expresiones.correo2, e.target, 'Correo2');
+		break;
+		case "password":
+			validarCampo(expresiones.password, e.target, 'Password');
+			
+		break;
+		
+
 		
 	}
 }
@@ -75,23 +48,7 @@ let validarCampo = (expresion, input, campo) => {
 	}
 }
 
-//Se valida que las dos contraseñas dadas sean validas
-let validarPassword2 = () => {
-	let inputPassword1 = document.getElementById('password');
-	let inputPassword2 = document.getElementById('passwordConfirmacion');
 
-	if(inputPassword1.value !== inputPassword2.value){
-		document.getElementById(`alertPasswordConfirm`).classList.add('alert-activa');
-		document.getElementById(`alertPasswordConfirm`).classList.remove('alert-noactiva');
-		
-		campos['password'] = false;
-	} else {
-		document.getElementById(`alertPasswordConfirm`).classList.remove('alert-activa');
-		document.getElementById(`alertPasswordConfirm`).classList.add('alert-noactiva');
-		
-		campos['password'] = true;
-	}
-}
 
 //Se comprueba cada que se realicen los siguientes eventos
 inputs.forEach((input) => {
