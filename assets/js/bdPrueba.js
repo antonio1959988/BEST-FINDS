0176967d8bfd6fd2aa96ddfa6430199df96ea8f4
AntonioTrrs <antonio1959988@hotmail.com>
2023-10-12 18:08:35 -0600

    implementacion de filtros

---
+++ assets/js/bdPrueba.js
@@ -0,0 +1,53 @@
+// Datos para la busqueda
+const productosArray = [
+    {
+        nombre: "Juanito",
+        descripcion: "descripcion",
+        precio: 324.45,
+        fecha: "2021-09-17",
+        categoria: "Ojos",
+        marca: "Rare Beauty",
+        colores: "coloresSeleccionados"
+    },
+
+    {
+        nombre: "Juanito",
+        descripcion: "descripcion",
+        precio: 99.56,
+        fecha: "2021-08-17",
+        categoria: "Ojos",
+        marca: "Rare Beauty",
+        colores: "coloresSeleccionados"
+    },
+
+    {
+        nombre: "Juanito",
+        descripcion: "descripcion",
+        precio: 10.99,
+        fecha: "2022-09-17",
+        categoria: "Rostro",
+        marca: "Urban Decay",
+        colores: "coloresSeleccionados"
+    },
+
+    {
+        nombre: "Juanito",
+        descripcion: "descripcion",
+        precio: 455.32,
+        fecha: "2021-09-10",
+        categoria: "Rostro",
+        marca: "Too Faced",
+        colores: "coloresSeleccionados"
+    },
+
+    {
+        nombre: "Juanito",
+        descripcion: "descripcion",
+        precio: 233.12,
+        fecha: "2021-11-17",
+        categoria: "Labios",
+        marca: "Colourpop",
+        colores: "coloresSeleccionados"
+    }
+
+];
