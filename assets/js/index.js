b47e031aa702e0f92d93e06510137939b9d96fe3
AntonioTrrs <antonio1959988@hotmail.com>
2023-10-20 11:22:00 -0600

    Mejora ls index

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,3 +11,70 @@ const myCarousel = new bootstrap.Carousel(carousel, {
     
 });
 
+//////////////////////
+// Recuperar los datos JSON del almacenamiento local
+var productosArray = JSON.parse(localStorage.getItem("productosArray"));
+console.log(productosArray);
+
+// Mostrar todos los productos al cargar la página
+document.addEventListener('DOMContentLoaded', () => {
+    mostrarProductos(productosArray);
+});
+
+var contadorId = 0;
+
+function mostrarProductos(productosArray) {
+
+    // Leer el elemento Resultado
+    const contenedor = document.querySelector('#resultado');
+
+
+
+            // Construir el HTML de los productos
+    productosArray.forEach(producto => {
+    
+        if(contadorId <= 3){
+            contadorId++;
+ 
+            const productoHTML = document.createElement('div');
+            productoHTML.className = "col-sm-6 col-md-6 col-lg-3";
+            productoHTML.innerHTML = `
+            <a href="./productos2.html" class="enlaceProducto" target="_self">
+            <div id="contenedorInferior">
+              <img src="./assets/img/Productos/${producto.nombre}/${producto.colores[0]}.webp" alt="producto" class="img-fluid"
+                id="imgProducto">
+              <br>
+              <span id="nombreProducto" class="nombreProducto">${producto.nombre}</span>
+              <span id="precio">$${producto.precio}</span>
+            </a>
+              <form>
+                <input type="number" class="form-control" id="cantidadProducto" min="0" max="100" value="0">
+                <button type="button" class="btn btn-primary agregar-carrito" id="btnAplicar" data-id="${contadorId}">Agregar al carrito</button>
+              </form>
+            </div>
+            `;
+            console.log(producto);
+            contenedor.appendChild(productoHTML);
+        }
+        
+
+    })
+} //codigo 
+    
+ 
+/*const myCarousel2 = new bootstrap.Carousel(carousel2, {
+    interval: interval,
+    
+});*/
+
+
+//Funcion para inicializar la sesión para el carrito 
+function inicializarSession() {
+    let productosArray = JSON.parse(sessionStorage.getItem("productosArray"));
+    if(!productosArray){
+        sessionStorage.setItem("productosArray", JSON.stringify(new Array()));
+    }
+
+}
+
+inicializarSession();
